a35106f631e5b334201855b0e815ae0691bc682f
abdulqdos <abdulqdosalabinie@gmail.com>
2025-04-19 13:50:50 +0200

    initial Filament

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Conference;
+use App\Models\Talk;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Conference::factory(10)->create();
+        Talk::factory()->create();
     }
 }
